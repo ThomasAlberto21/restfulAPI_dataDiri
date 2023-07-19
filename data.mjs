465f98f3d311e9cac20d2d50e7c85aa0ef8b8060
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-07-19 22:53:50 +0700

    Alamat belum tampil

---
+++ data.mjs
@@ -0,0 +1,20 @@
+const Data = [
+  {
+    nama: 'Agil',
+    tempat_lahir: 'Pontianak',
+    tgl_lahir: '1999-01-01',
+    jenis_kelamin: 'Laki-laki',
+    gol_darah: 'O',
+    alamat: {
+      jalan: 'Jl.A.Yani',
+      kota: 'Pontianak',
+      negara: 'Indonesia',
+    },
+    agama: 'Islam',
+    status: 'Belum Menikah',
+    pekerjaan: 'Mahasiswa',
+    kewarganegaraan: 'WNI',
+  },
+];
+
+export default Data;
--- service.mjs
+++ service.mjs
@@ -1,11 +1,9 @@
+import Data from './data.mjs';
+
 export class Service {
-  data = [
-    {
-      nama: 'Thomas',
-      umur: 19,
-      pekerjaan: 'Mahasiswa',
-    },
-  ];
+  constructor(){
+    this.data = Data;
+  }
 
   getJsonData() {
     return JSON.stringify({
@@ -15,8 +13,20 @@ export class Service {
         return {
           id: index,
           nama: value?.nama,
-          umur: value?.umur,
+          tempat_lahir: value?.tempat_lahir,
+          tgl_lahir: value?.tgl_lahir,
+          jenis_kelamin: value?.jenis_kelamin,
+          gol_darah: value?.gol_darah,
+
+          alamat: {
+            jalan: value?.jalan,
+            kota: value?.kota,
+            negara: value?.negara,
+          },
+          agama: value?.agama,
+          status: value?.status,
           pekerjaan: value?.pekerjaan,
+          kewarganegaraan: value?.kewarganegaraan,
         };
       }),
     });
